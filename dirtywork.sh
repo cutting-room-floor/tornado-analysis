5d55edb2efff7d65a7ec2746a10deca3dad7e9ca
Tom MacWright <tom@macwright.org>
2015-03-05 16:50:04 -0500

    Fancier, use d3

--- dirtywork.sh
+++ dirtywork.sh
@@ -1,9 +1,9 @@
-for f in out/*.png; do
-    gm convert $f $f.gif
-done
+# for f in out/*.png; do
+#     gm convert $f $f.gif
+# done
+# 
+# for f in out2/*.png; do
+#     gm convert $f $f.gif
+# done
 
-for f in out2/*.png; do
-    gm convert $f $f.gif
-done
-
-gifsicle --colors 256 --loop -d10 out/*.gif out2/*.gif > animation.gif
+gifsicle -O3 --colors 32 --loop -d5 out/*.gif out2/*.gif > animation.gif
--- simplify.js
+++ simplify.js
@@ -3,38 +3,43 @@ var fs = require('fs'),
     leftpad = require('leftpad');
 
 var input = require('./tornadoes.json');
+var counties = require('./counties.json');
 
-var height = 450;
+var albers = d3.geo.albersUsa();
 
-function scale(xy) {
-    return [
-        ~~((xy[0] + 128) * (720 / (124-59))), ~~((52 - xy[1]) * (height / (52-21)))
-    ];
-}
+var height = 550;
 
 function center(xy) {
     return [
-        ~~((xy[0][0] + xy[1][0]) / 2),
-        ~~((xy[0][1] + xy[1][1]) / 2)
+        ~~((xy[0][0] + xy[1][0]) / 2), ~~((xy[0][1] + xy[1][1]) / 2)
     ];
 }
 
 var simple = input.features.map(function(f) {
-    return [scale(f.geometry.coordinates[0]), scale(f.geometry.coordinates[1])];
-});
+    return [albers(f.geometry.coordinates[0]), albers(f.geometry.coordinates[1])];
+}).filter(function(f) { return f[0]; });
 
 var Canvas = require('canvas')
-  , canvas = new Canvas(720, height)
+  , canvas = new Canvas(900, height)
   , ctx = canvas.getContext('2d');
 
 ctx.fillStyle = '#000';
-ctx.fillRect(0, 0, 720, height);
+ctx.fillRect(0, 0, 900, height);
 var scale = simple.length;
 
 var color = d3.scale.linear()
     .range(["steelblue", "magenta"])
     .domain([0, simple.length])
     .interpolate(d3.interpolateHcl);
+
+var path = d3.geo.path()
+   .projection(albers)
+   .context(ctx);
+
+ctx.lineWidth = 1;
+ctx.strokeStyle = '#333';
+path(counties);
+ctx.stroke();
 
 ctx.lineWidth = 1;
 simple.forEach(function(s, i) {
@@ -54,6 +59,10 @@ ctx.fillRect(0, 0, 720, height);
 for (var i = 0; i < 100; i++) {
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, 720, height);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#222';
+    path(counties);
+    ctx.stroke();
     simple.forEach(function(s, j) {
         if (i > (j / simple.length * 100)) {
             var c = center(s);
